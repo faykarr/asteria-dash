adf625064e49f4121b2c93a65bca39062f7aa97a
faykarr <nasyathfaykar@gmail.com>
2026-03-18 20:46:50 +0700

    feat: initial design-system style for admin-dashboard

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,5 +7,20 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.start();
 
+// Import PerfectScrollbar
+import PerfectScrollbar from 'perfect-scrollbar';
+import 'perfect-scrollbar/css/perfect-scrollbar.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sidebarMenu = document.getElementById('sidebar-menu');
+  if (sidebarMenu) {
+    new PerfectScrollbar(sidebarMenu, {
+      wheelSpeed: 2,
+      wheelPropagation: true,
+      minScrollbarLength: 20
+    });
+  }
+});
+
 // Import datatables
 import './datatables.js';
